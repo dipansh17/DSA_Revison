e03e29e72d76b349e421e0700d52e36acc283cb0
dipansh17 <70329771+dipansh17@users.noreply.github.com>
2022-10-29 00:03:08 +0530

    Create SelectionSort.cpp
    Committing about Selection sort

---
+++ SelectionSort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selectionSort(int*arr,int n){
+    for(int i=0;i<n-1;i++){
+        int mini=i;
+        for(int j=i+1;j<n;j++){
+           if(arr[j]<arr[mini])mini=j;
+        }
+        swap(arr[i],arr[mini]);
+    }
+}
+int main(){
+    int n=5;
+    int arr[5]={4,5,2,1,3};
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    selectionSort(arr,n);
+    cout<<"after selection sort"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
